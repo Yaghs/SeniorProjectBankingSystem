a45ff82221f0e3113c8829edbb21854e8da98f37
michelle Parisi <parimm@farmingdale.edu>
2025-03-07 13:21:17 -0500

    edit and save for bio,text field

--- Profilepage/functions.js
+++ Profilepage/functions.js
@@ -2,6 +2,8 @@
 
 let profilepic = document.getElementById('profile-pic');
 let inputFile = document.getElementById('fileinput');
+const editButton = document.getElementById('Edit');
+const Bio=document.getElementById("Bio")
 
 profilepic.addEventListener("click",function (){
     inputFile.click()
@@ -12,4 +14,17 @@ inputFile.onchange = function (){
         profilepic.src = URL.createObjectURL(inputFile.files[0]);
 
 }
+
+editButton.addEventListener("click",()=>{
+  if(Bio.disabled){
+      Bio.removeAttribute("disabled"); //enable text
+      editButton.textContent="Save";
+  }else{
+      Bio.setAttribute("disabled",true);
+      editButton.textContent="Edit";
+  }
+
+})
+
+
 
