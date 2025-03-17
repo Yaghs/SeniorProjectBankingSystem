a5c75425771c30715e30291f8e19bddd042c4f26
michelle Parisi <parimm@farmingdale.edu>
2025-03-16 22:30:25 -0400

    follow button works(just need to combine the info to 'other profiles'/put in firebase)

---
+++ other_Profilepage/functions.js
@@ -0,0 +1,17 @@
+const Follow = document.getElementById('Follow');
+const Follow_button = document.getElementById('Follow_button');
+
+Follow_button.addEventListener('click', function () {
+
+    let current_Follower = parseInt(Follow.textContent.split(": ")[1]);
+
+    // Increment the follower count
+    if(current_Follower>=0){
+        current_Follower =(current_Follower===0)?current_Follower+1 :current_Follower-1;
+    }
+
+
+    // Update the text content
+    Follow.textContent = `Followers: ${current_Follower}`;
+});
+
